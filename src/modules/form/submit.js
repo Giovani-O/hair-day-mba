45d53c9058c5d6637c8c7ccf09f2f4912ce1662f
Giovani <giovani98@proton.me>
2024-08-21 19:58:14 -0300

    feat: Finish user interface logic

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 
 const form = document.querySelector('form')
+const clientName = document.getElementById('client')
 const selectedDate = document.getElementById('date')
 
 // Data de hoje
@@ -14,4 +15,25 @@ selectedDate.min = today
 
 form.onsubmit = (event) => {
   event.preventDefault()
+
+  try {
+    // Recupera o nome do cliente
+    const name = clientName.value.trim()
+    if (!name) return alert('Por favor, preencha o nome do cliente')
+
+    // Recupera a hora selecionada
+    const hourSelected = document.querySelector('.hour-selected')
+    if (!hourSelected) return alert('Por favor, selecione um horário')
+
+    // Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(':')
+
+    // Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, 'hour')
+
+    // Gera um ID
+    const id = new Date().getTime()
+  } catch (error) {
+    alert('Não foi possível realizar o agendamento')
+  }
 }
